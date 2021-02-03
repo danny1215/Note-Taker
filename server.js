ff9ce720f496af1b88847fef9783c799b5214a51
daniel mestawat <dannymamusha@gmail.com>
2021-02-03 11:37:57 -0600

    added a get for index .js, css

--- server.js
+++ server.js
@@ -1,49 +1,57 @@
 const express = require("express");
 const fs = require("fs");
+// shortid to generate a random short id used for storing and also deleting notes
 const shortId = require("shortid")
 const db = require("./Develop/db/db.json");
 const path = require("path");
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+// middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
 // routes
 
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/notes.html')));
 
 
+app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/assets/js/index.js')));
+
+
+app.get('/assets/css/styles.css', (req, res)=> res.sendFile(path.join(__dirname, '/Develop/public/assets/css/styles.css')));
+
 // for displaying notes
 
-app.get("/api/notes", function (req, res){
-    return res.json(db);
-});
+app.get("/api/notes",  (req, res) => res.json(db));
 
 
-// saving the notes the user has typed to db
+// saving the notes the user has typed to db using post method
 
 app.post("/api/notes", function(req, res){
     console.log(req.body);
-    var newNote = {
+    const newNote = {
         id: shortId.generate(),
         title: req.body.title,
         text: req.body.text,
     };
     console.log(newNote);
     db.push(newNote);
-    // fs.writeFile("./Develop/db/db.json", JSON.stringify(db), function(err){
-    // if (err) throw err;
-    // return res.json(db);
-    // });
+    fs.writeFile("./Develop/db/db.json", JSON.stringify(db), function(err){
+    if (err) throw err;
+    return res.json(db);
+    });
 
 });
 
+
+// deleting the note using delete method
 app.delete("/api/notes/:id", function (req, res){
     // console.log(req.params.id);
     var id =req.params.id;
@@ -57,5 +65,5 @@ app.delete("/api/notes/:id", function (req, res){
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+    console.log(`server is listening: http://localhost:${PORT}`);
   });
